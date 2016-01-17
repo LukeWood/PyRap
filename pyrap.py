4487b064fc4f08dd1a4139e5753c26abfdeb1c4a
lukewoodSMU <lswood@smu.edu>
2016-01-16 21:51:31 -0600

    big update

--- pyrap.py
+++ pyrap.py
@@ -2,8 +2,10 @@ from pybrain.datasets import SupervisedDataSet
 from pybrain.utilities import percentError
 from pybrain.tools.shortcuts import buildNetwork
 from pybrain.supervised.trainers import BackpropTrainer
-from pybrain.structure.modules import SoftmaxLayer
+from random import randint
+#END IMPORTS
 
+#FUNCTION DECLARATIONS BEGIN
 def wordinlist(word_list,tword):
 	for word in word_list:
 		if word == tword:
@@ -16,7 +18,7 @@ def converttoint(word_list,words):
 		try:
 			asint.append(word_list.index(word))
 		except Exception:
-			asint.append(0)
+			asint.append(-1)
 	return asint
 
 def inttowords(word_list,nums):
@@ -31,11 +33,14 @@ def inttowords(word_list,nums):
 				aswords.append(" ")
 	return aswords
 
+#FUNCTION DECLARATIONS END
+
 #BEGIN VARIABLE DECLARATIONS
 word_list = list()
-insize=3
-outsize = 10
+insize=2
+outsize = 50
 nwords = list()
+rapper_list = list()
 ds = SupervisedDataSet(insize,outsize)
 net = buildNetwork(insize,5,outsize)
 trainer = BackpropTrainer(net,ds)
@@ -50,28 +55,44 @@ with open('wordlist.txt') as f:
 			for word in subline.split():
 				word_list.append(word.strip())
 
+#fetching list of rappers
+with open('rapperlist.txt') as f:
+	content = f.readlines()
+	for line in content:
+		for subline in line.split():
+			for word in subline:
+				rapper_list.append(word.strip())
+
+#adding any unused words from the raps to nwords
 with open('raps.txt') as f:
 	content = f.readlines()
 	for line in content:
-		for subline in line.split(':'):
+		slines = line.split(":")
+		rappername = slines[0]
+		if not wordinlist(rapper_list,rappername):
+			rapper_list.append(rappername)
+		for subline in slines:
 			words = set(word.strip() for word in subline.split())
 			for word in words:
 				if not wordinlist(word_list,word.lower()):
 					word_list.append(word.lower())
 					nwords.append(word.lower())
+
+#adding them in to the wordlist file
 with open('wordlist.txt', 'a') as f:
 	for word in nwords:
 		f.write(word + " ")
 
+#opening up raps
 with open('raps.txt') as f:
 	contents = f.readlines()
 	for line in contents:
 			linesplit = line.split(":")
 			if(len(linesplit) ==2):
-				inp = linesplit[0].split()
-				inp = converttoint(word_list,inp)
+				inp = linesplit[0]
+				inp = converttoint(rapper_list,inp)
 				for i in range(0,insize-len(inp)):
-					inp.append(-1)
+					inp.append(randint(0,9))
 				rap = linesplit[1].split()
 				rap = converttoint(word_list,rap)
 				for i in range(0,outsize-len(rap)):
@@ -82,12 +103,12 @@ with open('raps.txt') as f:
 	
 trainer.trainUntilConvergence()
 
-n1 = int(input())
+print("Enter a rappers name from the list of rappers available:")
+n1 = input()
+print("Enter an integer between 0 and 9")
 n2 = int(input())
-n3 = int(input())
 
-output = net.activate((n1,n2,n3))
-print(output)
+output = net.activate((n1,n2))
 output = inttowords(word_list,output)
 print(output)
 
---
+++ rapconverter.py
@@ -0,0 +1,25 @@
+import sys
+import string
+
+if(len(sys.argv)!=3):
+	print("Please run this file with the syntax of: rapconv.py fname rappername")
+	sys.exit()
+fname = sys.argv[1]
+rname = sys.argv[2]
+lyrics = ""
+try:
+	with open(fname) as f:
+		contents = f.readlines()
+		for line in contents:
+			lyrics+=line
+except Exception:
+	print("INVALID FILENAME")
+	sys.exit()
+lyrics = lyrics.lower()
+lyrics = lyrics.replace("\n","")
+allc = string.maketrans("","")
+noweirdchars = allc.translate(allc,string.ascii_lowercase+ " ")
+lyrics = lyrics.translate(allc,noweirdchars)
+with open("raps.txt","a") as f
+	f.write(rname+":"+lyrics)
+
