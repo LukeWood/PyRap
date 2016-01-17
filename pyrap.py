1f458d11bf82acc6a94de906356446063a5a32d6
lukewoodSMU <lswood@smu.edu>
2016-01-16 21:13:39 -0600

    Cleaned up

--- pyrap.py
+++ pyrap.py
@@ -13,23 +13,40 @@ def wordinlist(word_list,tword):
 def converttoint(word_list,words):
 	asint = list()
 	for word in words:
-		asint.append(word_list.index(word))
+		try:
+			asint.append(word_list.index(word))
+		except Exception:
+			asint.append(0)
 	return asint
 
 def inttowords(word_list,nums):
 	aswords = list()
 	for i in nums:
-		aswords.append(word_list[int(i)])
+		try:
+			aswords.append(word_list[int(i)])
+		except Exception:
+			aswords.append(" ")
 	return aswords
+
+#BEGIN VARIABLE DECLARATIONS
 word_list = list()
+insize=3
+outsize = 6
+nwords = list()
+ds = SupervisedDataSet(insize,outsize)
+net = buildNetwork(insize,5,outsize)
+trainer = BackpropTrainer(net,ds)
+#END VARIABLE DECLARATIONS
+
+
+#fetching list of all possible words
 with open('wordlist.txt') as f:
 	content = f.readlines()
 	for line in content:
 		for subline in line.split(':'):
 			for word in subline.split():
 				word_list.append(word.strip())
-nwords = list()
-ds = SupervisedDataSet(3,3)
+
 with open('raps.txt') as f:
 	content = f.readlines()
 	for line in content:
@@ -47,15 +64,19 @@ with open('raps.txt') as f:
 	contents = f.readlines()
 	for line in contents:
 			linesplit = line.split(":")
-			inp = linesplit[0].split()
-			inp = converttoint(word_list,inp)
-			rap = linesplit[1].split()
-			rap = converttoint(word_list,rap)
-			ds.addSample(inp,rap)
-			
-net = buildNetwork(3,3,3)
-
-trainer = BackpropTrainer(net,ds)
+			if(len(linesplit) ==2):
+				inp = linesplit[0].split()
+				inp = converttoint(word_list,inp)
+				for i in range(0,3-len(inp)):
+					inp.append(0)
+				rap = linesplit[1].split()
+				rap = converttoint(word_list,rap)
+				for i in range(0,6-len(rap)):
+					rap.append(0)
+				if len(rap) == 6:
+					if len(inp) == 3:	
+						ds.addSample(inp,rap)
+	
 trainer.trainUntilConvergence()
 
 n1 = int(input())
@@ -66,3 +87,4 @@ output = net.activate((n1,n2,n3))
 
 output = inttowords(word_list,output)
 print(output)
+
